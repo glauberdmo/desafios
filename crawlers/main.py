1c7bf9bdcf2e20ba400f25ed060c7b6cf1b1efb1
glauberdmo <glauberdmo@gmail.com>
2021-11-13 00:34:15 -0300

    Challenge: refactoring

--- crawlers/main.py
+++ crawlers/main.py
@@ -1,5 +1,6 @@
 from reddit_bot.reddit_scraper import RedditScraper
 
+# uses this file to run through CLI
 if __name__=="__main__":
     Scraper = RedditScraper()
     Scraper.get_sub_reddit('rocketleague')
